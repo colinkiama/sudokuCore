24e2742deaba7e418ab063175a0bc0f741ecb505
Colin Kiama <colinkiama@gmail.com>
2019-06-13 11:26:30 +0100

    Moved back to one js file

--- sudokuCore.js
+++ sudokuCore.js
@@ -2,27 +2,230 @@
 const SUDOKUBOARDWIDTH = 9;
 const SUDOKUINNERGRIDWIDTH = 3;
 
-usableRows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-usableColumns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var unusedCells = [];
-var usedCells = [];
+var validCellsMap = new Map();
+var invalidCellsMap = new Map();
+var currentDigitCellsMap = new Map();
 
 // Classes
 class SudokuBoard {
   constructor() {
     this.board = create2DArray();
-    unusedCells = createUnusedCellsList();
+    this.unusedDigits = digits.slice();
+    fillMaps();
   }
 
   createNewGame() {
     // Step 1: Fill up grid with numbers considering sudoku constraints
     fillBoard.call(this);
+    console.log(this.board);
     // Step 2: Remove cells considering difficulty level.
   }
 }
 
-// Module Functions
+function test(){
+  for (let i = 0; i < 50; i++) {
+    var boardObj = new SudokuBoard();
+    boardObj.createNewGame();
+  }
+}
+
+function fillMaps() {
+  for (let i = 0; i < SUDOKUBOARDWIDTH; i++) 
+  {
+    // first two maps are digits to rows with an array of cells.
+    validCellsMap[digits[i]] = new Array(9);
+    invalidCellsMap[digits[i]] = new Array(9);
+
+    // this map is [digits] to array of cells
+    currentDigitCellsMap[digits[i]] = []
+
+    for (let j = 0; j < SUDOKUBOARDWIDTH; j++) {
+      validCellsMap[digits[i]][j] = [];
+      invalidCellsMap[digits[i]][j] = [];
+
+    }
+  }
+}
+
+function fillBoard() {
+  // foreach digit, loop through rows
+  this.unusedDigits.forEach(digit => {
+    // For each digit you'll be able to see cells that were valid for
+    // each row to avoid unnecessarily re-applying constraints
+    // when backtracking
+
+    for (let currentRow = 0; currentRow < SUDOKUBOARDWIDTH; currentRow++) {
+      validCellsMap[digit][currentRow] = getValidCellsForRow.call(this, currentRow, digit);
+
+      // Check if you need to backtrack
+      if (!validCellsMap[digit][currentRow].length > 0) {
+        var isBacktrackingRequired = true;
+
+        // check if you need to go back down a digit
+        var backRow = currentRow - 1;
+        var backDigit = digit;
+        if(currentRow - 1 == -1){
+          // It will go down to 8 in the method parameter
+          backRow = 8;
+
+          backDigit = digit - 1;
+
+        }
+
+        while (isBacktrackingRequired) {
+          backTrack.call(this, backDigit, backRow);
+          validCellsMap[digit][currentRow] = getValidCellsForRow.call(
+            this,
+            currentRow,
+            digit
+          );
+          isBacktrackingRequired = !validCellsMap[digit][currentRow].length > 0;
+        }
+      }
+      var indexToUse = selectRandomInt(validCellsMap[digit][currentRow].length);
+      var cellToUse = validCellsMap[digit][currentRow][indexToUse];
+      this.board[cellToUse.row][cellToUse.column] = digit;
+      // console.log(this.board);
+      currentDigitCellsMap[digit].push(cellToUse);
+    }
+  });
+}
+
+function backTrack(digit, backTrackRow) {
+  var failedCell = currentDigitCellsMap[digit].pop();
+
+  // console.log(this.board);
+
+  // You need to unset the cell
+  this.board[failedCell.row][failedCell.column] = null;
+  invalidCellsMap[digit][backTrackRow].push(failedCell);
+  validCellsMap[digit][backTrackRow].pop(failedCell);
+
+  // Check if you need to backtrack
+  if (!validCellsMap[digit][backTrackRow].length > 0) {
+    var isBacktrackingRequired = true;
+    invalidCellsMap[digit][backTrackRow] = [];
+
+    // check if you need to go back down a digit
+    var backRow = backTrackRow - 1;
+    var backDigit = digit;
+    if(backTrackRow - 1 == -1){
+      // It will go down to 8 in the method parameter
+      backRow = 8;
+
+      backDigit = digit - 1;
+
+    }
+    while (isBacktrackingRequired) {
+      backTrack.call(this, backDigit, backRow);
+      validCellsMap[digit][backTrackRow] = getValidCellsForRow.call(
+        this,
+        backTrackRow,
+        digit
+      );
+      isBacktrackingRequired = !validCellsMap[digit][backTrackRow].length > 0;
+    }
+  }
+  var indexToUse = selectRandomInt(validCellsMap[digit][backTrackRow].length);
+  var cellToUse = validCellsMap[digit][backTrackRow][indexToUse];
+  this.board[cellToUse.row][cellToUse.column] = digit;
+  currentDigitCellsMap[digit].push(cellToUse);
+}
+
+function selectRandomInt(upperBound) {
+  return Math.floor(Math.random() * Math.floor(upperBound));
+}
+
+// function shuffle(a) {
+//   var j, x, i;
+//   for (i = a.length - 1; i > 0; i--) {
+//     j = Math.floor(Math.random() * (i + 1));
+//     x = a[i];
+//     a[i] = a[j];
+//     a[j] = x;
+//   }
+//   return a;
+// }
+
+function getValidCellsForRow(currentRow, digit) {
+  var rowValues = this.board[currentRow];
+  var cellList = [];
+
+  for (let i = 0; i < rowValues.length; i++) {
+    // 3rd constraint (cellsAlreadyFilledIn)
+    if (rowValues[i] == null) {
+      cellList.push({ row: currentRow, column: i });
+    }
+  }
+
+  // 1st constraint (cells used previously in higher rows)
+  if (currentDigitCellsMap[digit].length > 0) {
+    cellList = cellList.filter(isNotInSameColumnAsUsedDigitCells, digit);
+
+    // 2nd constraint (Regions of previous cells)
+
+    if (currentRow % SUDOKUINNERGRIDWIDTH > 0) {
+      // This row isn't a new region so this constraint will apply.
+      cellList = cellList.filter(isNotInSameRegionAsUsedDigitCells, digit);
+    }
+  }
+
+  // Backtrack constraint (Is not in invalid cells)
+  if (invalidCellsMap[digit][currentRow].length > 0) {
+    cellList = cellList.filter(isNotInInvalidCellsList, [currentRow, digit]);
+  }
+
+  return cellList;
+}
+
+function isNotInInvalidCellsList(cell) {
+  var currentRow = this[0];
+  var digit = this[1];
+  var isNotInInvalid = true;
+  if (invalidCellsMap[digit][currentRow].includes(cell)) {
+    isNotInInvalid = false;
+  }
+  return isNotInInvalid;
+}
+
+function isNotInSameRegionAsUsedDigitCells(cell) {
+  // Step 1: Get used cells in the current region [x]
+  // Step 2: Loop through the cells in the current region
+  // Step 3: Compare each cell in the current region with
+  // the cell you are testing.
+  var isNotInSameRegion = true;
+  var digit = this;
+  var currentRegionToCheck = Math.floor(currentDigitCellsMap[digit].length / SUDOKUINNERGRIDWIDTH);
+  var startIndexToInclude = currentRegionToCheck * SUDOKUINNERGRIDWIDTH;
+  var endBoundIndex = startIndexToInclude + SUDOKUINNERGRIDWIDTH;
+  var usedCellsInCurrentRegion = currentDigitCellsMap[digit].slice(startIndexToInclude, endBoundIndex);
+
+  for (let i = 0; i < usedCellsInCurrentRegion.length; i++) {
+    if (
+      Math.floor(currentDigitCellsMap[digit][i].column / SUDOKUINNERGRIDWIDTH) ==
+      Math.floor(cell.column / SUDOKUINNERGRIDWIDTH)
+    ) {
+      isNotInSameRegion = false;
+      break;
+    }
+  }
+  return isNotInSameRegion;
+}
+
+function isNotInSameColumnAsUsedDigitCells(cell) {
+  var isNotInSameColumn = true;
+  var digit = this;
+  for (let i = 0; i < currentDigitCellsMap[digit].length; i++) {
+    if (currentDigitCellsMap[digit][i].column == cell.column) {
+      isNotInSameColumn = false;
+      break;
+    }
+  }
+  return isNotInSameColumn;
+}
+
 function create2DArray() {
   // JavaScript doesn't have a built-in way of creating
   // 2D arrays so you literally create an
@@ -33,237 +236,4 @@ function create2DArray() {
   }
 
   return outerArray;
-}
-
-function createUnusedCellsList() {
-  var unusedCellCoordinates = [];
-  for (let i = 0; i < SUDOKUBOARDWIDTH; i++) {
-    for (let j = 0; j < SUDOKUBOARDWIDTH; j++) {
-      unusedCellCoordinates.push({ row: i, column: j });
-    }
-  }
-
-  return unusedCellCoordinates;
-}
-
-function fillBoard() {
-  // Sudoku rules considered
-  // 1. A row must not have repeated digits.
-  // 2. A column must not have repeated digits
-  // 3. Inner 3x3 regions must not have repeated digits
-
-  // for (let i = 0; i < SUDOKUBOARDWIDTH * SUDOKUBOARDWIDTH; i++) {
-  //   var selectedCell = selectCellRandomly();
-  //   console.log(selectedCell);
-  //   var usableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    
-  //     usableNums = checkIfRowRepeats.call(this, selectedCell, usableNums);
-
-  //     usableNums = checkIfColumnRepeats.call(this, selectedCell, usableNums);
-
-  //     usableNums = checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-
-  //     // checkIfRowRepeats.call(this, selectedCell, usableNums);
-  //     // checkIfColumnRepeats.call(this, selectedCell, usableNums);
-  //     // checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-
-  //   var numToUse = usableNums[getRandomInt(usableNums.length)];
-  //   this.board[selectedCell.row][selectedCell.column] = numToUse;
-  // }
-
-  for (let i = 0; i < SUDOKUBOARDWIDTH; i++) {
-    for (let j = 0; j < SUDOKUBOARDWIDTH; j++) {
-      var selectedCell = {row: i, column: j};
-      console.log(selectedCell);
-
-      var usableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      
-      if(i == 0){
-        usableNums = shuffle(usableNums);
-      }
-
-    
-      usableNums = checkIfRowRepeats.call(this, selectedCell, usableNums);
-
-      usableNums = checkIfColumnRepeats.call(this, selectedCell, usableNums);
-
-      usableNums = checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-
-      // checkIfRowRepeats.call(this, selectedCell, usableNums);
-      // checkIfColumnRepeats.call(this, selectedCell, usableNums);
-      // checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-      if (!usableNums.length > 0){
-        var isBacktrackingRequired = true;
-        var backTrackIndex = 1;
-        while(isBacktrackingRequired){
-          backTrack.call(this, backTrackIndex, getLastCellFromCurrentCell(selectedCell));
-          
-          usableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-          usableNums = checkIfRowRepeats.call(this, selectedCell, usableNums);
-          
-          usableNums = checkIfColumnRepeats.call(this, selectedCell, usableNums);
-          
-          usableNums = checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-          
-          isBacktrackingRequired = !usableNums.length > 0;
-          
-          backTrackIndex = backTrackIndex + 1;
-        }
-
-      }
-      var numToUse = usableNums[0];
-      this.board[selectedCell.row][selectedCell.column] = numToUse;
-      console.log(this.board);
-    }
-    
-  }
-
-  console.log(this.board);
-}
-
-function getLastCellFromCurrentCell(selectedCell){
-  var lastCellRow = selectedCell.row;  
-  var lastCellColumn = selectedCell.column - 1;
-    if(lastCellColumn < 0){
-        lastCellRow = lastCellRow - 1;
-        lastCellColumn = SUDOKUBOARDWIDTH - 1;
-    }
-  
-  return {row: lastCellRow, column: lastCellColumn};
-}
-
-function backTrack(backTrackIndex, selectedCell){
-  console.log("Cell on backtrack: " + selectedCell.row + ", " + selectedCell.column);
-  this.board[selectedCell.row][selectedCell.column] = null;
-  var usableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  usableNums = checkIfRowRepeats.call(this, selectedCell, usableNums);
-
-  usableNums = checkIfColumnRepeats.call(this, selectedCell, usableNums);
-
-  usableNums = checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-  
-  // cuts list down to numbers not used yet.
-  usableNums.splice(0, backTrackIndex)
-
-  if (!usableNums.length > 0){
-    var isBacktrackingRequired = true;
-    var backTrackIndex = 1;
-    while(isBacktrackingRequired){
-      backTrack.call(this, backTrackIndex, getLastCellFromCurrentCell(selectedCell));
-    
-      usableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-      usableNums = checkIfRowRepeats.call(this, selectedCell, usableNums);
-
-      usableNums = checkIfColumnRepeats.call(this, selectedCell, usableNums);
-
-      usableNums = checkIf3x3RegionRepeats.call(this, selectedCell, usableNums);
-      
-      isBacktrackingRequired = !usableNums.length > 0;
-      
-      backTrackIndex = backTrackIndex + 1;
-    }
-
-  }
-
-  
-  var numToUse = usableNums[0];
-  this.board[selectedCell.row][selectedCell.column] = numToUse;
-
-}
-
-
-function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
-  }
-  return a;
-}
-
-function getRandomInt(upperBound) {
-  return Math.floor(Math.random() * Math.floor(upperBound));
-}
-
-function selectCellRandomly() {
-  console.log("Unused Cells Length : " + unusedCells.length);
-  var cellIndex = Math.floor(Math.random() * Math.floor(unusedCells.length));
-  var selectedCell = unusedCells[cellIndex];
-  unusedCells.splice(cellIndex, 1);
-  return selectedCell;
-}
-
-function checkIfRowRepeats(selectedCell, usableNumList) {
-  var rowArray = [];
-  for (let i = 0; i < SUDOKUBOARDWIDTH; i++) {
-    var itemToPush = this.board[selectedCell.row][i];
-    if (itemToPush != null) {
-      rowArray.push(itemToPush);
-    }
-  }
-  // checkForRepeatsOnSection(rowArray, usableNumList);
-  return checkForRepeatsOnSection(rowArray, usableNumList);
-}
-
-function checkIfColumnRepeats(selectedCell, usableNumList) {
-  var columnArray = [];
-  for (let i = 0; i < SUDOKUBOARDWIDTH; i++) {
-    var itemToPush = this.board[i][selectedCell.column];
-    if (itemToPush != null) {
-      columnArray.push(itemToPush);
-    }
-  }
-  // checkForRepeatsOnSection(columnArray, usableNumList);
-  return checkForRepeatsOnSection(columnArray, usableNumList);
-}
-
-function checkIf3x3RegionRepeats(selectedCell, usableNumList) {
-  var regionArray = [];
-
-  // 1. get 3x3 board region coords
-  // 2. get local (0,0) of 3x3 board region
-  // 3. get cells in region
-  // 4. add each cell to a list to check for repeats 
-
-  var regionRow = selectedCell.row % SUDOKUINNERGRIDWIDTH;
-  var regionColumn = selectedCell.column % SUDOKUINNERGRIDWIDTH;
-
-   // You multiply by 3 so you get the local (0,0) of each grid: (3,0), (6,3) etc.
-  var localOriginRow = regionRow * SUDOKUINNERGRIDWIDTH;
-  var localOriginColumn = regionColumn * SUDOKUINNERGRIDWIDTH;
-
- 
-  for (let i = localOriginRow; i < localOriginRow + SUDOKUINNERGRIDWIDTH; i++) {
-    for (let j = localOriginColumn; j < localOriginColumn + SUDOKUINNERGRIDWIDTH; j++) {
-      var itemToPush = this.board[i][j];
-      if (itemToPush != null) {
-        regionArray.push(itemToPush);
-      }
-    }
-  }
-
-  // checkForRepeatsOnSection(regionArray, usableNumList);
-  return checkForRepeatsOnSection(regionArray, usableNumList);
-}
-
-function checkForRepeatsOnSection(sectionList, usableNums) {
-  // Reverse for-loop makes it easier to remove items from an array
-  var startIndex = usableNums.length - 1
-  for (let i = startIndex; i > -1; i--) {
-    var num = usableNums[i];
-    if (sectionList.includes(num)) {
-      // if(usableNums.length == 1){
-      //   console.log("final stand");
-      // }
-      usableNums.splice(i,1);
-      // console.log(usableNums);
-    }
-  }
-  return usableNums;
 }
